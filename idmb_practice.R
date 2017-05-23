79cb4b6e6e1f89d218567efca12676111e76a4c7
Harry Yang <harry.seunghoon.yang@gmail.com>
2017-05-23 20:33:17 +0900

    finished the quest

--- idmb_practice.R
+++ idmb_practice.R
@@ -11,6 +11,8 @@ idmb %>%
   
 str(idmb)
 
+# actor_name
+
 ratingdat <- idmb %>% 
   group_by(actor_1_name) %>% 
   summarise(M = mean(imdb_score, na.rm=T), 
@@ -30,3 +32,22 @@ ggplot(ratings, aes(x = M, xmin = M-SE, xmax = M+SE, y = actor_1_name )) +
   theme(axis.text=element_text(size=8)) +
   xlab("Mean rating") + ylab("First Actor") 
 
+#director_names
+
+ratingsdat <- idmb %>% 
+  group_by(director_name) %>% 
+  summarise(M = mean(imdb_score, na.rm=T),
+            SE = sd(imdb_score, na.rm=T)/sqrt(length(na.omit(imdb_score))),
+            N = length(na.omit(imdb_score)))
+
+ratings2 <- filter(ratingsdat, N>=10 & !(director_name==''))
+
+ratings2$director_name <- factor(ratings2$director_name)
+ratings2$director_name <- reorder(ratings2$director_name, ratings2$M)
+
+ggplot(ratings2, aes(x = M, xmin=M-SE, xmax=M+SE, y=director_name)) +
+  geom_point() +
+  geom_segment( aes(x = M-SE, xend=M+SE,
+                    y= director_name, yend=director_name)) +
+  theme(axis.text=element_text(size=8)) +
+  xlab("Mean rating") + ylab("Main Director")
