00584f169a4c2c5b9e1e100345d92e47b114144a
harryyang1982 <harry.seunghoon.yang@gmail.com>
2017-05-29 10:46:30 +0900

    edit

--- 20170529 lecture.R
+++ 20170529 lecture.R
@@ -121,3 +121,42 @@ head(sort(wordcount, decreasing=T), 30)
 palete <- brewer.pal(9, "Set3")
 wordcloud(names(wordcount), freq=wordcount, scale=c(5,1), rot.per=0.25, min.freq=2, random.order = F, random.color=T, colors=palete)
 
+# 제주도 추천 여행코스 찾기
+
+install.packages("stringr")
+library(KoNLP)
+library(wordcloud)
+library(tidyverse)
+library(stringr)
+
+useSejongDic()
+mergeUserDic(data.frame(readLines("제주도여행지.txt", encoding = "UTF-8")))
+
+txt <- readLines("jeju.txt", encoding="UTF-8")
+place <- sapply(txt, extractNoun, USE.NAMES = F)
+place
+head(unlist(place), 30)
+cdata <- unlist(place)
+place <- str_replace_all(cdata, "[^[:alpha:]]", "")
+place <- gsub(" ", "", place)
+
+txt <- readLines("제주도여행코스gsub.txt", encoding="UTF-8")
+txt
+
+cnt_txt <- length(txt)
+i <- 1
+for(i in 1:cnt_txt) {
+  place <- gsub((txt[i]), "", place)
+}
+
+
+place <- Filter(function(x) {nchar(x) >= 2}, place)
+write(unlist(place), "jeju_2.txt")
+rev <- read.table("jeju_2.txt")
+nrow(rev)
+wordcount <- table(rev)
+head(sort(wordcount, decreasing=T), 30)
+
+library(RColorBrewer)
+palete <- brewer.pal(9, "Set1")
+wordcloud(names(wordcount), freq=wordcount, scale=c(5,1), rot.per=0.25, min.freq=2, random.order=F, random.color=T, colors=palete)
