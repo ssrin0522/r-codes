f36bf6772ad94062e1152132b9a198cbcf505e6c
harryyang1982 <harry.seunghoon.yang@gmail.com>
2017-05-29 10:29:05 +0900

    files

--- 20170529 lecture.R
+++ 20170529 lecture.R
@@ -59,3 +59,65 @@ library(RColorBrewer)
 palete <- brewer.pal(9, "Set3")
 wordcloud(names(wordcount), freq=wordcount, scale=c(5,1), rot.per=0.25, min.freq=1, random.order=F, random.color = T, colors=palete)
 legend(0.3, 1, "서울시 응답소 요청사항 분석", cex=0.8, fill=NA, border=NA, bg="white", text.col="red", text.font=2, box.col="red")
+
+
+# 성형외과 질문 게시판 분석
+
+## 사전 불러오기
+useSejongDic()
+
+data1 <- read_lines("remake.txt")
+data1
+
+data2 <- sapply(data1, extractNoun, USE.NAMES = F)
+data2
+data3 <- unlist(data2)
+data3 <- Filter(function(x) {nchar(x) <= 10}, data3)
+
+head(unlist(data3), 30)
+
+data3 <- gsub("\\d+", "", data3)
+data3 <- gsub("쌍수", "쌍꺼풀", data3)
+data3 <- gsub("쌍커풀", "쌍꺼풀", data3)
+data3 <- gsub("메부리코", "매부리코", data3)
+data3 <- gsub("\\.", "", data3)
+data3 <- gsub(" ", "", data3)
+data3 <- gsub("\\'", "", data3)
+
+data3
+
+write(unlist(data3), "remake_2.txt")
+data4 <- read.table("remake_2.txt")
+data4
+nrow(data4)
+wordcount <- table(data4)
+wordcount
+
+head(sort(wordcount, decreasing=T), 20)
+
+data4
+
+txt <- readLines("성형gsub.txt")
+txt
+cnt_txt <- length(txt)
+
+i <- 1
+for (i in 1:cnt_txt) {
+  data3 <- gsub((txt[i]), "", data3)
+}
+
+data3
+data3 <- Filter(function(x) {nchar(x) >=2}, data3)
+write(unlist(data3), "remake_2.txt")
+data4 <- read.table("remake_2.txt")
+data4
+nrow(data4)
+
+wordcount <- table(data4)
+wordcount
+
+head(sort(wordcount, decreasing=T), 30)
+
+palete <- brewer.pal(9, "Set3")
+wordcloud(names(wordcount), freq=wordcount, scale=c(5,1), rot.per=0.25, min.freq=2, random.order = F, random.color=T, colors=palete)
+
